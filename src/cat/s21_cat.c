f613938210b5767176eb4d712f8d7da372e770fe
Meadows Ellis <meadowse@et-a6.kzn.21-school.ru>
2022-06-13 23:18:18 +0300

    final

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,8 +1,8 @@
 #include "s21_cat.h"
 
 int main(int argc, char *argv[]) {
-    int rez, b = 0, e = 0, n = 0, s = 0, t = 0, T = 0, E = 0;
-    const char* short_options = "beEstTn";
+    int rez = 0, b = 0, e = 0, n = 0, s = 0, t = 0, v = 0, exit = 0;
+    const char* short_options = "beEstTnv";
     const struct option long_options[] = {
         { "number-nonblank", no_argument, NULL, 'b' },
         { "number", no_argument, NULL, 'n' },
@@ -11,87 +11,71 @@ int main(int argc, char *argv[]) {
     };
     while ((rez = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
         switch (rez) {
-            case 'b': {b = 1; break;};
-            case 'e': {e = 1; break;};
-            case 'E': {E = 1; break;};
-            case 'n': {n = 1; break;};
-            case 's': {s = 1; break;};
-            case 't': {t = 1; break;};
-            case 'T': {T = 1; break;};
+            case 'b': b = 1; break;
+            case 'e': e = 1; v = 1; break;
+            case 'E': e = 1; break;
+            case 'n': n = 1; break;
+            case 's': s = 1; break;
+            case 't': t = 1; v = 1; break;
+            case 'T': t = 1; break;
+            case 'v': v = 1; break;
+            default:
+            printf("cat: illegal option -- %c\n", rez);
+            printf("usage: cat [-benstuv] [file ...]\n");
+			exit = 1;
+			break;
         }
     }
-    open_file(argv, b, e, n, s, t, T, E);
+    if (!exit) {
+		while (optind < argc) {
+			open_file(argv, b, e, n, s, t, v);
+			optind++;
+		}
+	}
     return 0;
 }
 
-void open_file(char* argv[], int b, int e, int n, int s, int t, int T, int E) {
-    FILE* fp;
-    argv++;
-    int result = 1;
-    while (*argv) {
-        if (*argv[0] == '-') {
-            argv++;
-        } else {
-            if ((fp = fopen(*argv, "r")) == NULL) {
-                argv++;
-            } else {
-                result = s21_cat(b, e, n, s, t, T, E, fp, result);
-                argv++;
-            }
-        }
+void open_file(char* argv[], int b, int e, int n, int s, int t, int v) {
+    FILE* fp = NULL;
+    if (b && n) n = 0;
+    fp = fopen(argv[optind], "r+");
+    if (!fp) {
+		printf("cat: %s: %s\n", argv[optind], strerror(errno));
+    } else {
+        s21_cat(b, e, n, s, t, v, fp);
+        fclose(fp);
     }
 }
 
-int s21_cat(int b, int e, int n, int s, int t, int T, int E, FILE* fp, int count) {
-    int current, prev = '\n';
-    int temp = 0;
-    while ((current = getc(fp)) != EOF) {
+void s21_cat(int b, int e, int n, int s, int t, int v, FILE* fp) {
+    int current, prev = 1, temp = 0, count = 1, first = 1;
+    while ((current = fgetc(fp)) != EOF) {
         if (s) {
             if (current == '\n') temp++;
             if (current != '\n') temp = 0;
         }
-        if (b && temp < 3) {
-            if ((prev == '\n' && !(current == '\n')) || count == 1) {
-                printf("%6d\t", count);
-                count++;
-            }
-        }
-        if (n && !b && temp < 3) {
-            if (prev == '\n' || count == 1) {
-                printf("%6d\t", count);
-                count++;
-            }
-        }
-        if ((e || E) && temp < 3) {
-            if ((t || e) && current >= 0 && current < 32 && current != '\n' && current != '\t') {
-                printf("^%c", 64 + current);
-            } else if ((t || T) && current == '\t') {
-                printf("^I");
-            } else if (current != '\n') {
+        if (current == '\n' && (!s || temp < 3)) {
+				if (n && prev == '\n')
+                    printf("%6d\t", count++);
+                if (e)
+                    printf("$");
                 printf("%c", current);
+			}
+        if (current != '\n') {
+                if ((prev == '\n' || first == 1) && (n || b))
+                    printf("%6d\t", count++);
+                if (current < 32 && current != 9 && current != 10 && v)
+                    printf("^%c", current + 64);
+                else if (current > 127 && current < 160 && v)
+                    printf("M-^%c", current - 64);
+                else if (current == 127 && v)
+                    printf("^%c", current - 64);
+                else if (current == '\t' && t)
+                    printf("^I");
+                else
+                    printf("%c", current);
             }
-            if (current == '\n') {
-                printf("$\n");
-            }
-            if (current != '\n') temp = 0;
-        } else if ((t || T) && temp < 3) {
-            if (!e && t && current >= 0 && current < 32 && current != '\n' && current != '\t') {
-                printf("^%c", 64 + current);
-            } else if (current != '\t' && current != '\n') {
-                printf("%c", current);
-            } else if (!e && !E && current == '\n') {
-                printf("\n");
-            }
-            if (current == '\t') {
-                printf("^I");
-            }
-            if (current != '\n') temp = 0;
-        } else {
-            if (!((e || E) && current == '\n') && temp < 3) {
-                putchar(current);
-            }
-        }
         prev = current;
+        first = 0;
     }
-    return count;
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <errno.h>
 
-void open_file(char* argv[], int b, int e, int n, int s, int t, int T, int E);
-int s21_cat(int b, int e, int n, int s, int t, int T, int E, FILE* fp, int count);
+void open_file(char* argv[], int b, int e, int n, int s, int t, int v);
+void s21_cat(int b, int e, int n, int s, int t, int v, FILE* fp);
 
 #endif  //  SRC_CAT_S21_CAT_H_
